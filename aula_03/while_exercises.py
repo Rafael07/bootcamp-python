98f423b8571d75f95695d99b9a84980135891d90
Rafael <rafaelrodriguesdsa@gmail.com>
2025-09-29 14:57:52 -0300

    Parte do exercicio feito

--- aula_03/while_exercises.py
+++ aula_03/while_exercises.py
@@ -3,11 +3,24 @@
 ### Exercício 11. Leitura de Dados até Flag
 # Ler dados de entrada até que uma palavra-chave específica ("sair") seja fornecida.
 
+# entrada = ''
+# while entrada != 'sair':
+#     entrada = input("Digite uma palavra: ").lower()
+
 ### Exercício 12. Validação de Entrada
 # Solicitar ao usuário um número dentro de um intervalo específico até que a entrada seja válida.
 
+# numero = int(input("Insira um número de 1 a 10: "))
+
+# while numero not in range(1,11):
+#     print("Número fora do intervalo!")
+#     numero = int(input("Insira novamente um número de 1 a 10: "))
+# print("Numero válido!")
+    
 ### Exercício 13. Consumo de API Simulado
 # Simular o consumo de uma API paginada, onde cada "página" de dados é processada em loop até que não haja mais páginas.
+
+
 
 ### Exercício 14. Tentativas de Conexão
 # Simular tentativas de reconexão a um serviço com um limite máximo de tentativas.
