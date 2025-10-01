c174424f15438617af511eb29ad16bae13d02c69
Rafael07 <rafaelrodriguesdsa@gmail.com>
2025-10-01 05:39:38 -0300

    Exercícios While

--- aula_03/while_exercises.py
+++ aula_03/while_exercises.py
@@ -20,10 +20,39 @@
 ### Exercício 13. Consumo de API Simulado
 # Simular o consumo de uma API paginada, onde cada "página" de dados é processada em loop até que não haja mais páginas.
 
+# last_page = 13
+# page = 1
 
-
+# while True:
+#     print(f"Página {page} da API...")
+#     page += 1
+#     if page > last_page:
+#         print("Não há mais páginas na API!")
+#         break
+        
 ### Exercício 14. Tentativas de Conexão
 # Simular tentativas de reconexão a um serviço com um limite máximo de tentativas.
 
+# tentativas_maximas = 5
+# tentativa = 1
+
+# while tentativa <= tentativas_maximas:
+#     print(f"Tentativa {tentativa} de {tentativas_maximas}")
+#     tentativa += 1
+#     print("Tentando reconectar ao serviço...")
+    
+# print("Falha ao conectar após várias tentativas.")
+
 ### Exercício 15. Processamento de Dados com Condição de Parada
 # Processar itens de uma lista até encontrar um valor específico que indica a parada.
+
+lista = [1,2,3,4,5,'parar',6,8,9]
+indice = 0
+
+while indice <= len(lista):
+    if lista[indice] == 'parar':
+        print("Processo encerrado...")
+        break
+    print(f"Processando item {lista[indice]}")
+    indice += 1
+        
