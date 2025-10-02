70faad587b62028e71bab0eb74adb837a7d66180
Rafael <rafaelrodriguesdsa@gmail.com>
2025-10-02 13:45:09 -0300

    desafio com type hints

--- aula_04/desafio.py
+++ aula_04/desafio.py
@@ -1,17 +1,17 @@
-def calculo_bonus(nome, salario, porcentagem):
-    kpi_bonus = 1000 + (salario * porcentagem)
+def calculo_bonus(nome: str, salario: float, porcentagem: float):
+    kpi_bonus: float = 1000 + (salario * porcentagem)
     return (f"Olá {nome}, seu bonus foi de {kpi_bonus} reais")
     
 
 def main():
 
-    nome_valido = False
-    salario_valido = False
-    porcentagem_valida = False
+    nome_valido: bool = False
+    salario_valido: bool = False
+    porcentagem_valida: bool = False
    
     while not nome_valido:
         try:    
-            nome = input("Digite seu nome: ")
+            nome: str = input("Digite seu nome: ")
             if len(nome) == 0:
                  raise ValueError("Erro: o nome não pode estar vazio.")
             elif any(char.isdigit() for char in nome):
@@ -23,7 +23,7 @@ def main():
             
     while not salario_valido:
             try:
-                salario = float(input("Digite seu salario: "))
+                salario: float = float(input("Digite seu salario: "))
                 if salario <= 0:
                     print("Erro: o salário não pode ser igual ou inferior a zero.")
                 else:
@@ -33,7 +33,7 @@ def main():
     
     while not porcentagem_valida:
             try:
-                porcentagem = float(input("Digite sua porcentagem de bonus: "))
+                porcentagem: float = float(input("Digite sua porcentagem de bonus: "))
                 if porcentagem <= 0:
                      print("Erro: a porcentagem não pode ser igual ou inferior a zero.") 
                 else:
