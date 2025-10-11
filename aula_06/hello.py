8c0c915faa4eb2c6fbceaadcbf499118965258fe
Rafael07 <rafaelrodriguesdsa@gmail.com>
2025-10-11 09:13:03 -0300

    fix: adapta o código de acordo com PEP8

--- aula_06/hello.py
+++ aula_06/hello.py
@@ -1,12 +1,12 @@
-from csv import reader
-from collections import defaultdict
 import time
-
+from collections import defaultdict
+from csv import reader
 from pathlib import Path
 
 
 def processar_temperaturas(path_do_txt: Path):
-    print('Iniciando o processamento do arquivo.')
+
+    print("Iniciando o processamento do arquivo.")
     start_time = time.time()  # Tempo de início
 
     temperatura_por_station = defaultdict(list)
@@ -34,13 +34,13 @@ def processar_temperaturas(path_do_txt: Path):
         temperatura_por_station[nome_da_station].append(temperatura)
     """
 
-    with open(path_do_txt, 'r', encoding='utf-8') as file:
-        _reader = reader(file, delimiter=';')
+    with open(path_do_txt, "r", encoding="utf-8") as file:
+        _reader = reader(file, delimiter=";")
         for row in _reader:
             nome_da_station, temperatura = str(row[0]), float(row[1])
             temperatura_por_station[nome_da_station].append(temperatura)
 
-    print('Dados carregados. Calculando estatísticas...')
+    print("Dados carregados. Calculando estatísticas...")
 
     # Dicionário para armazenar os resultados calculados
     results = {}
@@ -52,27 +52,27 @@ def processar_temperaturas(path_do_txt: Path):
         max_temp = max(temperatures)
         results[station] = (min_temp, mean_temp, max_temp)
 
-    print('Estatística calculada. Ordenando...')
+    print("Estatística calculada. Ordenando...")
     # Ordenando os resultados pelo nome da estação
     sorted_results = dict(sorted(results.items()))
 
     # Formatando os resultados para exibição
     formatted_results = {
-        station: f'{min_temp:.1f}/{mean_temp:.1f}/{max_temp:.1f}'
+        station: f"{min_temp:.1f}/{mean_temp:.1f}/{max_temp:.1f}"
         for station, (min_temp, mean_temp, max_temp) in sorted_results.items()
     }
 
     end_time = time.time()  # Tempo de término
-    print(f'Processamento concluído em {end_time - start_time:.2f} segundos.')
+    print(f"Processamento concluído em {end_time - start_time:.2f} segundos.")
 
     return formatted_results
 
 
 # Substitua "data/measurements10M.txt" pelo caminho correto do seu arquivo
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # 1M 0.38 segundos
     # 10M 3.96 segundos.
-    path_do_txt: Path = Path('data/measurements.txt')
+    path_do_txt: Path = Path("data/measurements.txt")
     # 100M > 5 minutos.
     resultados = processar_temperaturas(path_do_txt)
